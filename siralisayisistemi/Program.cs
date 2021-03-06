978be0111fd8680501ecb2199ad6c58aad05d4c4
muhamed gündoğan <rey_36_63@hotmail.com>
2021-03-06 14:31:56 +0300

    Proje dosyası ekle.

---
+++ siralisayisistemi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siralisayisistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kaç adet sayı üretelim? ");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.Write("başlangıç değeri= ");
+            int ilk = int.Parse(Console.ReadLine());
+            Console.Write("son değeri= ");
+            int son = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int[] sayilar = new int[sayi];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = rnd.Next(ilk, son);
+                for (int k = 0; k < i; k++)
+                {
+                    if (sayilar[i] == sayilar[k])
+                    {
+                        i--;
+                        break;
+                    }
+
+                }
+            }
+            Array.Sort(sayilar);
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine($"-{sayilar[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ siralisayisistemi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("siralisayisistemi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("siralisayisistemi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("eecbe9ab-acc1-41ed-8e4c-1e342cedf917")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
